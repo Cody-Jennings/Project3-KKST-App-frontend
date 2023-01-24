d20ce22731d96b44a2058a464420517c6cb33a3c
Cody Jennings <cojack91@gmail.com>
2023-01-24 16:15:03 -0500

    createpagebugfix2

--- src/components/MultiKKSTData.js
+++ src/components/MultiKKSTData.js
@@ -1,32 +1,35 @@
 import { getMultiKKSTData } from '../services/kkst-api'
-import{ useState, useEffect } from 'react'
-import IndvKKSTData from './IndvKKSTData'
+import { useState, useEffect } from 'react'
 import Create from './CreateKKSTData'
 import { Link } from 'react-router-dom'
 
 export default function MultiKKSTData() {
-    const [multiKKSTData, setMultiKKSTData] = useState([])
-    useEffect(()=>{
-        getMultiKKSTData() // calling the function to get the data
-        .then(res => setMultiKKSTData(res.data))
-    }, [])
+  const [multiKKSTData, setMultiKKSTData] = useState([])
+  useEffect(() => {
+    getMultiKKSTData() // calling the function to get the data
+      .then(res => setMultiKKSTData(res.data))
+  }, [])
 
 
-    console.log(multiKKSTData)
-    return(
-        
-      <div>
+  console.log(multiKKSTData)
+  return (
+
+    <div>
       <h1>Welcome to the Kool Kid Slang translator site</h1>
-      
-    {multiKKSTData.sort((a,b) => a.Phrase.localeCompare(b.Phrase)).map((IndvKKSTData) => {
-      return (
-          <Link to={`/${IndvKKSTData._id}`}>
-              <h3>
-              {IndvKKSTData.Phrase}
-            </h3></Link>
-      )
-    })}
-    <Create />
-  </div>
-    )
+      <ul>
+        {multiKKSTData.sort((a, b) => a.Phrase.localeCompare(b.Phrase)).map((IndvKKSTData) => {
+          return (
+            <li>
+              <Link to={`/${IndvKKSTData._id}`}>
+                <h3>
+                  {IndvKKSTData.Phrase}
+                </h3>
+              </Link>
+            </li>
+          )
+        })}
+      </ul>
+      <Create />
+    </div>
+  )
 }
